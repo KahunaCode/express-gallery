5c5a77833e1f6f9ca19fd3f3b1827f4ea3b844c4
James Moore <jameslmoore@gmail.com>
2017-08-05 20:11:33 -1000

    updated with titles

--- gallery.sql
+++ gallery.sql
@@ -5,6 +5,6 @@
 -- \c gallery
 
 INSERT INTO "Galleries" VALUES
-(default, 'james the author', 'https://static.pexels.com/photos/20974/pexels-photo.jpg', 'this is my cool thing', NOW(), NOW()),
-(default, 'bob another author', 'https://www.ncl.com/sites/default/files/DestinationGalleries.Hawaii.SnorkelingBay900x400.jpg', 'i have a cool thing too', NOW(), NOW()),
-(default, 'steve also author', 'http://www.nationalgeographic.com/content/dam/travel/2017-digital/hawaii/top-ten/6-coast-north-shore-kauai-hawaii.jpg', 'i have a third thing', NOW(), NOW());
+(default, 'Woodland Wonderland','james', 'https://static.pexels.com/photos/20974/pexels-photo.jpg', 'this is my cool thing', NOW(), NOW()),
+(default, 'Beautiful Bay', 'bob', 'https://www.ncl.com/sites/default/files/DestinationGalleries.Hawaii.SnorkelingBay900x400.jpg', 'i have a cool thing too', NOW(), NOW()),
+(default, 'North Shore', 'steve', 'http://www.nationalgeographic.com/content/dam/travel/2017-digital/hawaii/top-ten/6-coast-north-shore-kauai-hawaii.jpg', 'i have a third thing', NOW(), NOW());
