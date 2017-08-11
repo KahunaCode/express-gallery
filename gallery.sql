bf3ce57faae7f9ab050e65f8f22b61aa85057720
James Moore <jameslmoore@gmail.com>
2017-08-10 21:29:06 -1000

    better test data

--- gallery.sql
+++ gallery.sql
@@ -5,6 +5,6 @@
 -- \c gallery
 
 INSERT INTO "Galleries" VALUES
-(default, 'Woodland Wonderland','james', 'https://static.pexels.com/photos/20974/pexels-photo.jpg', 'this is my cool thing', NOW(), NOW()),
-(default, 'Beautiful Bay', 'bob', 'https://www.ncl.com/sites/default/files/DestinationGalleries.Hawaii.SnorkelingBay900x400.jpg', 'i have a cool thing too', NOW(), NOW()),
-(default, 'North Shore', 'steve', 'http://www.nationalgeographic.com/content/dam/travel/2017-digital/hawaii/top-ten/6-coast-north-shore-kauai-hawaii.jpg', 'i have a third thing', NOW(), NOW());
+(default, 'Woodland Wonderland','james', 'https://static.pexels.com/photos/20974/pexels-photo.jpg', 'Well paint one happy little tree right here. Lets put some happy little clouds in our world. You can do anything here - the only pre-requisite is that it makes you happy. Lets go up in here, and start having some fun In your imagination you can go anywhere you want.', NOW(), NOW()),
+(default, 'Beautiful Bay', 'bob', 'https://www.ncl.com/sites/default/files/DestinationGalleries.Hawaii.SnorkelingBay900x400.jpg', 'Even trees need a friend. We all need friends. This is an example of what you can do with just a few things, a little imagination and a happy dream in your heart. Lets make some happy little clouds in our world. Anyone can paint. You cant make a mistake. Anything that happens you can learn to use - and make something beautiful out of it. Let your heart take you to wherever you want to be.', NOW(), NOW()),
+(default, 'North Shore', 'steve', 'http://www.nationalgeographic.com/content/dam/travel/2017-digital/hawaii/top-ten/6-coast-north-shore-kauai-hawaii.jpg', 'You gotta think like a tree. The light is your friend. Preserve it. Dont forget to tell these special people in your life just how special they are to you. You better get your coat out, this is going to be a cold painting. All you have to learn here is how to have fun. La- da- da- da- dah. Just be happy.', NOW(), NOW());
